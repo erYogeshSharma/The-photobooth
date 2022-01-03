22cf9a6a3d5510d22cf8460710cddc5cc4278184
yogesh <er.yogeshsharma505@gmail.com>
2022-01-03 18:28:22 +0530

    final commit

---
+++ Serve/routes/posts.js
@@ -0,0 +1,14 @@
+import express from 'express';
+ 
+import {deletePost,getPostsBySearch, getPost ,likePost, getPosts, createPost, updatePost} from '../controllers/posts.js';
+import auth from "../middleware/auth.js"
+const router = express.Router();
+
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+router.post('/',auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id',auth, deletePost);
+router.patch('/:id/likePost',auth, likePost);
+export default router; 
