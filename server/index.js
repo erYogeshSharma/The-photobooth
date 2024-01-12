9b2411f32183fe9676bfcabc5c2b3c160d7d532a
Yogesh Sharma <yogeshsharma@Yogeshs-MacBook-Air.local>
2024-01-13 00:10:37 +0530

    MongoURI

--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/users', userRoutes);
-app.use('/posts', postRoutes); 
- 
+app.use('/posts', postRoutes);
 
-app.get('/',(req,res) =>{
+
+app.get('/', (req, res) => {
   res.send('welcome to The photobooth')
 });
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 6000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
